919855292606d186959e88cf84cfbfcb133fd9cb
Jesus Oroya <41575404+djoroya@users.noreply.github.com>
2024-12-17 18:26:51 +0100

    can be change simulation folder

--- runstep/simpath.py
+++ runstep/simpath.py
@@ -3,5 +3,14 @@ import os,sys
 
 folder = os.path.dirname(__file__)
 
+if "RUNSTEP_FOLDER" in os.environ:
+    folder = os.environ["RUNSTEP_FOLDER"]
+    # mkdir folder
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    # simulations
+    if not os.path.exists(os.path.join(folder,"simulations")):
+        os.makedirs(os.path.join(folder,"simulations"))
+
 def simpath():
     return os.path.join(folder,"simulations")
